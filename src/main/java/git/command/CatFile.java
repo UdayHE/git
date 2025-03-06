4b630baa4a8d75b72750c7f019b44a187ad19249
Uday <iamudayhegde@gmail.com>
2025-03-06 12:13:40 +0530

    Modified Cat-File command

--- src/main/java/git/command/CatFile.java
+++ src/main/java/git/command/CatFile.java
@@ -15,16 +15,20 @@ public class CatFile implements Command {
     @Override
     public void execute(String[] args) {
         if (args[1].equals("-p")) {
-            String fileName = args[2];
-            String path = String.format(".git/objects/%s/%s", fileName.substring(0, 2), fileName.substring(2));
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(path))))) {
-                String line = reader.readLine();
-                System.out.print(line.substring(line.indexOf('\0') + 1));
-                while ((line = reader.readLine()) != null)
-                    log.log(Level.INFO,line);
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
+            catFile(args);
+        }
+    }
+
+    private void catFile(String[] args) {
+        String fileName = args[2];
+        String path = String.format(".git/objects/%s/%s", fileName.substring(0, 2), fileName.substring(2));
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(path))))) {
+            String line = reader.readLine();
+            System.out.print(line.substring(line.indexOf('\0') + 1));
+            while ((line = reader.readLine()) != null)
+                log.log(Level.INFO, line);
+        } catch (IOException exception) {
+            log.log(Level.SEVERE, exception.getMessage());
         }
     }
 }
