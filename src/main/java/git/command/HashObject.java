36210691202ea91ff2545d449ce2cbec9b12143a
Uday <iamudayhegde@gmail.com>
2025-03-06 16:29:47 +0530

    Added hash-object command

--- src/main/java/git/command/HashObject.java
+++ src/main/java/git/command/HashObject.java
@@ -18,9 +18,6 @@ public class HashObject implements Command {
 
     private static final String ARG = "-w";
     private static final String SHA_1 = "SHA-1";
-    private static final String BLOB = "blob ";
-    private static final String NULL_CHAR = "\0";
-    private static final String HEX_FORMAT = "%02x";
     private static final String OBJECTS_PATH = ".git/objects/";
 
     private static final byte[] OBJECT_TYPE_BLOB = "blob".getBytes();
@@ -29,44 +26,36 @@ public class HashObject implements Command {
 
     @Override
     public void execute(String[] args) throws Exception {
-//        if (args.length != 2 || !ARG.equals(args[1])) {
-//            log.log(Level.SEVERE, "Usage: hash-object -w <file>");
-//            return;
-//        }
+        if (args.length != 2 || !ARG.equals(args[1])) {
+            log.log(Level.SEVERE, "Usage: hash-object -w <file>");
+            return;
+        }
         String fileName = args[2];
         String hash = hashFile(new File(fileName));
         System.out.println(hash);
     }
 
     public String hashFile(File File) throws IOException, NoSuchAlgorithmException {
-        try (
-                var inputStream = new FileInputStream(File);
-        ) {
+        try (var inputStream = new FileInputStream(File)) {
             return hashFile(inputStream.readAllBytes());
         }
     }
 
-
-    public File getObjectsDirectory() {
-        return new File(new File(".git"), "objects");
-    }
-
     public String hashFile(byte[] bytes) throws IOException, NoSuchAlgorithmException {
         var lengthBytes = String.valueOf(bytes.length).getBytes();
-        var message = MessageDigest.getInstance("SHA-1");
+        var message = MessageDigest.getInstance(SHA_1);
         message.update(OBJECT_TYPE_BLOB);
         message.update(SPACE);
         message.update(lengthBytes);
         message.update(NULL);
         message.update(bytes);
 
-
         var hashBytes = message.digest();
         var hash = HexFormat.of().formatHex(hashBytes);
 
         var firstTwo = hash.substring(0, 2);
         var rest = hash.substring(2);
-        var firstTwoPath = Paths.get(getObjectsDirectory().getPath(), firstTwo).toFile();
+        var firstTwoPath = Paths.get(OBJECTS_PATH, firstTwo).toFile();
         firstTwoPath.mkdirs();
         var restPath = Paths.get(firstTwoPath.getPath(), rest).toFile();
         try (
