dfd1fb329d50715484c74b19801e6498e44a8025
Uday <iamudayhegde@gmail.com>
2025-03-06 16:07:45 +0530

    Added hash-object command

--- src/main/java/git/command/HashObject.java
+++ src/main/java/git/command/HashObject.java
@@ -1,10 +1,13 @@
 package git.command;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.DeflaterOutputStream;
@@ -20,6 +23,10 @@ public class HashObject implements Command {
     private static final String HEX_FORMAT = "%02x";
     private static final String OBJECTS_PATH = ".git/objects/";
 
+    private static final byte[] OBJECT_TYPE_BLOB = "blob".getBytes();
+    private static final byte[] SPACE = " ".getBytes();
+    private static final byte[] NULL = {0};
+
     @Override
     public void execute(String[] args) throws Exception {
         if (args.length != 2 || !ARG.equals(args[0])) {
@@ -28,70 +35,54 @@ public class HashObject implements Command {
         }
         String fileName = args[2];
 
-        try {
 
-            long fileSize = Files.size(Paths.get(fileName));
+    }
 
-            byte[] fileContentInByte = Files.readAllBytes(Paths.get(fileName));
-
-            String fileContentCovertedIntoString = new String(fileContentInByte);
-
-            String header = "blob " + fileSize + "\0";
-
-            String combinedData = header + fileContentCovertedIntoString;
-
-//           Now we have the combined data and need to convert it into hash
-
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-
-            byte[] messageDigest = md.digest(combinedData.getBytes());
-
-//           Convert byte array into hex format
-
-            StringBuilder sb = new StringBuilder();
-
-            for(byte b: messageDigest){
-
-                sb.append(String.format("%02x", b));
-
-            }
-
-            String hashedString = sb.toString();
-
-            String blobPath =
-
-                    String.format(".git/objects/%s/%s", hashedString.substring(0, 2),
-
-                            hashedString.substring(2));
-
-            File blobFile = new File(blobPath);
-
-            blobFile.getParentFile().mkdirs();
-
-            DeflaterOutputStream out =
-
-                    new DeflaterOutputStream(new FileOutputStream(blobFile));
-
-            out.write("blob".getBytes());
-
-            out.write(" ".getBytes());
-
-            out.write(String.valueOf(fileSize).getBytes());
-
-            out.write(0);
-
-            out.write(fileContentInByte);
-
-            out.close();
-
-            System.out.println(hashedString);
-
+    public String hashFile(File File) throws IOException, NoSuchAlgorithmException {
+        try (
+                var inputStream = new FileInputStream(File);
+        ) {
+            return hashFile(inputStream.readAllBytes());
         }
+    }
 
-        catch (Exception e) {
+    public File getDotGit() {
+        return new File( new File("."), ".git");
+    }
 
-            throw new Exception(e);
+    public File getObjectsDirectory() {
+        return new File(getDotGit(), "objects");
+    }
 
+    public String hashFile(byte[] bytes) throws IOException, NoSuchAlgorithmException {
+        var lengthBytes = String.valueOf(bytes.length).getBytes();
+        var message = MessageDigest.getInstance("SHA-1");
+        message.update(OBJECT_TYPE_BLOB);
+        message.update(SPACE);
+        message.update(lengthBytes);
+        message.update(NULL);
+        message.update(bytes);
+
+
+        var hashBytes = message.digest();
+        var hash = HexFormat.of().formatHex(hashBytes);
+
+        var firstTwo = hash.substring(0, 2);
+        var rest = hash.substring(2);
+        var firstTwoPath = Paths.get(getObjectsDirectory().getPath(), firstTwo).toFile();
+        firstTwoPath.mkdirs();
+        var restPath = Paths.get(firstTwoPath.getPath(), rest).toFile();
+        try (
+                var outputStream = Files.newOutputStream(restPath.toPath());
+                var deflaterOutputStream = new DeflaterOutputStream(outputStream);
+        ) {
+            deflaterOutputStream.write(OBJECT_TYPE_BLOB);
+            deflaterOutputStream.write(SPACE);
+            deflaterOutputStream.write(lengthBytes);
+            deflaterOutputStream.write(NULL);
+            deflaterOutputStream.write(bytes);
         }
+        return hash;
+
     }
 }
