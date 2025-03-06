c133b729f7fcb94bbee92d3788e492aa881911e4
Uday <iamudayhegde@gmail.com>
2025-03-06 13:59:48 +0530

    Modified Cat-File command

--- src/main/java/git/command/CatFile.java
+++ src/main/java/git/command/CatFile.java
@@ -8,23 +8,29 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.InflaterInputStream;
 
+import static java.lang.System.out;
+
 public class CatFile implements Command {
 
     private static final Logger log = Logger.getLogger(CatFile.class.getName());
 
+    private static final String ARG = "-p";
+    private static final String BASE_PATH = ".git/objects/%s/%s";
+    private static final char NULL_CHAR = '\0';
+
+
     @Override
     public void execute(String[] args) {
-        if (args[1].equals("-p")) {
+        if (args[1].equals(ARG))
             catFile(args);
-        }
     }
 
     private void catFile(String[] args) {
         String fileName = args[2];
-        String path = String.format(".git/objects/%s/%s", fileName.substring(0, 2), fileName.substring(2));
+        String path = String.format(BASE_PATH, fileName.substring(0, 2), fileName.substring(2));
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(path))))) {
             String line = reader.readLine();
-            System.out.print(line.substring(line.indexOf('\0') + 1));
+            out.print(line.substring(line.indexOf(NULL_CHAR) + 1));
             while ((line = reader.readLine()) != null)
                 log.log(Level.INFO, line);
         } catch (IOException exception) {
