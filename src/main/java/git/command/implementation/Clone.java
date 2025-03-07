fd014e53e2fb96308e352062f61fe8f22f463a06
Uday <iamudayhegde@gmail.com>
2025-03-07 10:35:09 +0530

    Modified clone command

--- src/main/java/git/command/implementation/Clone.java
+++ src/main/java/git/command/implementation/Clone.java
@@ -1,10 +1,9 @@
 package git.command.implementation;
 
 import git.command.Command;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,19 +31,36 @@ public class Clone implements Command {
             return;
         }
 
-        clone(repoUrl, repoDirectory, destinationDir);
+        clone(repoUrl, repoDirectory);
     }
 
-    private void clone(String repoUrl, File repoDirectory, String destinationDir) {
+    private void clone(String repoUrl, File repoDirectory) {
         try {
-            Git.cloneRepository()
-                    .setURI(repoUrl)
-                    .setDirectory(repoDirectory)
-                    .call();
-            log.log(Level.INFO, "Repository successfully cloned into: {0}", destinationDir);
-        } catch (GitAPIException e) {
+            // Construct the Git clone command
+            String[] command = {"git", "clone", repoUrl, repoDirectory.getAbsolutePath()};
+
+            // Execute the command
+            Process process = Runtime.getRuntime().exec(command);
+
+            // Wait for the command to finish
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                log.log(Level.INFO, "Repository successfully cloned into: {0}", repoDirectory.getAbsolutePath());
+            } else {
+                log.log(Level.SEVERE, "Error: Cloning failed. Exit code: {0}", exitCode);
+                // Optionally, you can read the error output from the process
+                // to log more detailed error messages.
+                readErrorOutput(process);
+            }
+        } catch (IOException | InterruptedException e) {
             log.log(Level.SEVERE, "Error: Cloning failed - {0}", e.getMessage());
         }
     }
-}
 
+    private void readErrorOutput(Process process) throws IOException {
+        java.util.Scanner s = new java.util.Scanner(process.getErrorStream()).useDelimiter("\\A");
+        String errorOutput = s.hasNext() ? s.next() : "";
+        log.log(Level.SEVERE, "Error output: {0}", errorOutput);
+    }
+}
