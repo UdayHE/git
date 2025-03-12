a233eef4205ce368a495c2dbf6504d2f6c27501a
Uday <iamudayhegde@gmail.com>
2025-03-12 21:43:54 +0530

    Refactored

--- src/main/java/git/constant/Constant.java
+++ src/main/java/git/constant/Constant.java
@@ -5,20 +5,19 @@ public class Constant {
     private Constant() {
     }
 
-    public static final String ARG_P = "-p";
-    public static final String ARG_M = "-m";
-    public static final String OBJECTS_BASE_PATH = ".git/objects/%s/%s";
     public static final char NULL_CHAR = '\0';
     public static final char EMPTY_CHAR = ' ';
-
-    public static final String SHA_1 = "SHA-1";
-    public static final String OBJECTS_PATH = ".git/objects/";
 
     public static final byte[] OBJECT_TYPE_BLOB = "blob".getBytes();
     public static final byte[] SPACE_BYTES = " ".getBytes();
     public static final byte[] HEADS_REFS_BYTES = "ref: refs/heads/main\n".getBytes();
     public static final byte[] NULL_BYTES = {0};
 
+    public static final String ARG_P = "-p";
+    public static final String ARG_M = "-m";
+    public static final String OBJECTS_BASE_PATH = ".git/objects/%s/%s";
+    public static final String SHA_1 = "SHA-1";
+    public static final String OBJECTS_PATH = ".git/objects/";
     public static final String FORWARD_SLASH = "/";
     public static final String HEX_CHAR = "%02x";
     public static final String BLOB = "blob ";
